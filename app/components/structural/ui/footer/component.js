19cd7ffdfde2ad137c25a51440c6e8510dc9758b
Ryan Roderick <ryan@designmonster.com>
2020-12-31 12:40:27 -0500

    Initial commit

---
+++ app/components/structural/ui/footer/component.js
@@ -0,0 +1,8 @@
+import Component from '@glimmer/component';
+
+export default class StructuralUiFooterComponent extends Component {
+    navLabelOne = 'Home';
+    navLabelTwo = 'Home';
+    navLabelThree = 'Home';
+    navLabelFour = 'Home';
+}
